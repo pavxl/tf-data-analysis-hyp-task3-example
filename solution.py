02c73eeb393e7530736a74e440ccd08d0ffa188c
pavxl <76624639+pavxl@users.noreply.github.com>
2023-04-13 16:30:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import cramervonmises_2samp
+from scipy.stats import ttest_ind
 
 chat_id = 362844815
 
-def solution(x: np.array, y: np.array) -> bool:
-    result = cramervonmises_2samp(x, y)
-    statistic, pvalue = result.statistic, result.pvalue
-    alpha = 0.01
-    if pvalue > alpha:
-        return True
-    else:
-        return False
+def solution(x, y) -> bool:
+    p_value = ttest_ind(x, y, alternative="greater").pvalue
+    return p_value < 0.03
